ddc79da2f4e0ad4fa416a3b22daf383b4a4a620b
LouisOlouis <Lcoliveiraaguiar@gmail.com>
2026-02-09 11:22:42 -0300

    Initial Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Rpg-8</title>
+    <link rel="stylesheet" href="Style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<header class="Top">
+    <div class="Container">
+        <a class="title" href="index.php"><b>OmegaOn</b></a>
+    </div>
+</header>
